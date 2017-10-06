6b458f19f504090e5bf9920c5b2e979d540255e1
izconcept <izkevin52@gmail.com>
2017-10-06 10:31:34 -0400

    fixed scroll bug

--- javascript/index.js
+++ javascript/index.js
@@ -1,11 +1,17 @@
-function scrollTo(id){
-    console.log(id);
-    $('html,body').animate({
-            scrollTop: $("#"+id).offset().top},
-        'slow');
-}
+// function scrollTo(){
+//     console.log('test');
+//     // $('html,body').animate({
+//     //         scrollTop: $("#"+id).offset().top},
+//     //     'slow');
+// }
 
 
 $(document).ready(function() {
     //$('.parallelBG').parallax({imageSrc: 'media/bg6.jpg', naturalWidth: "1280", naturalHeight: "1280", speed: "0.1"});
+    $(".menu-item").on('click', function() {
+    	var scrollID = $(this).data('scroll')
+    	$('html,body').animate({
+            scrollTop: $("#"+scrollID).offset().top},
+        'slow');
+	    })
 })
