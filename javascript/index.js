6bb56f8eb1cce5d080fa1d21b17d4319ccf5df69
izconcept <izkevin52@gmail.com>
2017-10-15 13:04:48 -0400

    updated CV

--- javascript/index.js
+++ javascript/index.js
@@ -1,11 +1,3 @@
-// function scrollTo(){
-//     console.log('test');
-//     // $('html,body').animate({
-//     //         scrollTop: $("#"+id).offset().top},
-//     //     'slow');
-// }
-
-
 $(document).ready(function() {
     //$('.parallelBG').parallax({imageSrc: 'media/bg6.jpg', naturalWidth: "1280", naturalHeight: "1280", speed: "0.1"});
     $(".menu-item").on('click', function() {
